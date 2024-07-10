b972008c146024db468a71c6030b6f37c772cc66
mcbarinov <mcbarinov@proton.me>
2024-07-10 12:28:14 +0300

    Start grpc server #7

--- src/command/start.rs
+++ src/command/start.rs
@@ -1,19 +1,61 @@
 use std::fs::File;
 
 use daemonize::Daemonize;
+use tokio::net::UnixListener;
+use tokio_stream::wrappers::UnixListenerStream;
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+
+use crate::timer_grpc::timer_service_server::{TimerService, TimerServiceServer};
+use crate::timer_grpc::{Empty, TimerStatus};
+
+#[derive(Debug, Default)]
+pub struct RpcService {}
+
+#[tonic::async_trait]
+impl TimerService for RpcService {
+    async fn status(&self, _request: Request<Empty>) -> Result<Response<TimerStatus>, Status> {
+        Ok(Response::new(TimerStatus { status: "status".to_string() }))
+    }
+
+    async fn pause(&self, _request: Request<Empty>) -> Result<Response<TimerStatus>, Status> {
+        Ok(Response::new(TimerStatus { status: "pause".to_string() }))
+    }
+
+    async fn resume(&self, _request: Request<Empty>) -> Result<Response<TimerStatus>, Status> {
+        Ok(Response::new(TimerStatus { status: "resume".to_string() }))
+    }
+
+    async fn stop(&self, _request: Request<Empty>) -> Result<Response<TimerStatus>, Status> {
+        Ok(Response::new(TimerStatus { status: "stop".to_string() }))
+    }
+}
 
 pub fn run(interval: u64) {
     println!("start a new timer with interval: {}", interval);
     let stdout = File::create("/tmp/ptimer.out").unwrap();
     let stderr = File::create("/tmp/ptimer.err").unwrap();
     let daemon = Daemonize::new().stdout(stdout).stderr(stderr).pid_file("/tmp/ptimer.pid");
+
     match daemon.start() {
         Ok(_) => {
             println!("Daemon started");
-            std::thread::sleep(std::time::Duration::from_secs(interval));
+            tokio::runtime::Runtime::new().unwrap().block_on(start_grpc_server(interval)).unwrap();
         }
         Err(e) => {
             eprintln!("Error, {}", e)
         }
     }
+}
+
+async fn start_grpc_server(_interval: u64) -> Result<(), Box<dyn std::error::Error>> {
+    let path = "/tmp/ptimer.sock";
+    let _ = tokio::fs::remove_file(path).await; // TODO: ?
+    let uds = UnixListener::bind(path)?;
+    let uds_stream = UnixListenerStream::new(uds);
+
+    println!("Listening on: {}", path);
+    Server::builder().add_service(TimerServiceServer::new(RpcService {})).serve_with_incoming(uds_stream).await?;
+
+    Ok(())
 }
