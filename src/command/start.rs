a04238743328d129ef9106d5c5a7f1245f54914a
mcbarinov <mcbarinov@proton.me>
2024-07-10 11:36:51 +0300

    start: daemonize #5

--- src/command/start.rs
+++ src/command/start.rs
@@ -1,3 +1,19 @@
-pub fn run(_interval: u64) {
-    println!("start")
+use std::fs::File;
+
+use daemonize::Daemonize;
+
+pub fn run(interval: u64) {
+    println!("start a new timer with interval: {}", interval);
+    let stdout = File::create("/tmp/ptimer.out").unwrap();
+    let stderr = File::create("/tmp/ptimer.err").unwrap();
+    let daemon = Daemonize::new().stdout(stdout).stderr(stderr).pid_file("/tmp/ptimer.pid");
+    match daemon.start() {
+        Ok(_) => {
+            println!("Daemon started");
+            std::thread::sleep(std::time::Duration::from_secs(interval));
+        }
+        Err(e) => {
+            eprintln!("Error, {}", e)
+        }
+    }
 }
