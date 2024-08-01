dea46c57dc2f81c7b0438740b5381a8d5a2a2a5c
mcbarinov <mcbarinov@proton.me>
2024-08-01 09:18:49 +0300

    System notifications are invisible in fullscreen #34

--- src/notification.rs
+++ src/notification.rs
@@ -4,7 +4,8 @@ use crate::config::Config;
 
 pub fn send_notification(config: &Config, message: &str) {
     let title = if config.debug_mode { "mm-pomodoro-dev" } else { "mm-pomodoro" };
-    let script = format!("display notification \"{message}\" with title \"{title}\" sound name \"Basso\"");
+    let mut script = format!("display notification \"{message}\" with title \"{title}\" sound name \"Basso\"");
+    script.push_str("\ndisplay dialog \"mm-pomodoro\" buttons {\"OK\"} default button \"OK\"");
     let output = Command::new("osascript").arg("-e").arg(script).output().expect("Failed to execute command");
 
     if output.status.success() {
